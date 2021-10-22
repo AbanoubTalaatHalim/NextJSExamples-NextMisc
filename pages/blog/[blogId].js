47af41cac40a09ecd9ee1fda51cac055c615ca98
Abanoub <abanoubtalaat30@gmail.com>
2021-10-22 15:22:13 +0200

    first commit

---
+++ pages/blog/[blogId].js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+function Blog({ title, description }) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Head>
+      <h1 className="content">
+        Env Analytics {process.env.NEXT_PUBLIC_ANALYTICS_ID}
+      </h1>
+    </>
+  );
+}
+
+export default Blog;
+/*
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { blogId: "1" } }],
+    fallback: false,
+  };
+}
+*/
+export async function getServerSideProps() {
+  const user = process.env.DB_USER;
+  const password = process.env.DB_PASSWORD;
+  console.log(
+    `Connecting to the database with username ${user} and password ${password}`
+  );
+  return {
+    props: {
+      title: "Article Title",
+      description: "Article Description",
+    },
+  };
+}
---
+++ pages/pets.js
@@ -0,0 +1,6 @@
+import img from "../public/1.jpg";
+function PetsPage() {
+  return <div>Pets </div>;
+}
+
+export default PetsPage;
